7d6c2439d9762d465a4470a1dc7cc287547fe320
Tuyan T <tuyantat@gmail.com>
2025-07-06 16:17:45 +0200

    Navsection + Element generation
    Added some functionality to the NavSection and figuring out how to read and and generate content from files.

--- script.js
+++ script.js
@@ -17,18 +17,52 @@ document.addEventListener("DOMContentLoaded", function () {
   //Loop over all the main options in the navList and add click functions to them
   navListMainSelections.forEach(function (mainSelection) {
     mainSelection.addEventListener("click", function () {
-      console.log(mainSelection.textContent);
+      const fileName = mainSelection.textContent.trim().toLowerCase();
 
-      //Swap between collapsed arrow and normal arrow on click
+      //Check if menu option is a drop-down if so swap arrow icon
       if (mainSelection.querySelector(".navListIcon i")) {
         const icon = mainSelection.querySelector(".navListIcon i");
+
         if (icon) {
           icon.classList.toggle("fa-chevron-right");
           icon.classList.toggle("fa-chevron-down");
         }
-      } else {
-        const fileName = mainSelection.textContent.trim() + ".html";
-        fetch("html files/" + fileName)
+
+        // Fetch the list of files in the subdirectory and put them in an array
+        fetch("html files/docs/" + fileName + "/")
+          .then((response) => response.text())
+          .then((html) => {
+            // Parse the directory listing (assuming it's an HTML page)
+            const tempDiv = document.createElement("div");
+            tempDiv.innerHTML = html;
+            const links = tempDiv.querySelectorAll("a");
+            const files = [];
+            links.forEach((link) => {
+              const href = link.getAttribute("href");
+              // Filter out parent directory links and directories
+              if (href && href !== "../" && href.endsWith(".html")) {
+                // Get the last part of the href (filename), remove .html extension if present
+                let file = href.split("/").pop();
+                file = file.replace(".html", "");
+                files.push(file);
+              }
+            });
+            console.log(files);
+            //Loop throug the generated file array to generate menu listings for all the files
+            files.forEach((file) => {
+              const navigationListElement = document.createElement("span");
+              navigationListElement.className = "navigationListElement";
+              navigationListElement.innerText = file;
+
+              document
+                .getElementById(fileName + "ListDiv")
+                .appendChild(navigationListElement);
+            });
+          });
+      }
+      //If menu option is not a drop down then open related file in content section
+      else {
+        fetch("html files/" + fileName + ".html")
           .then((response) => response.text())
           .then((html) => {
             contentDiv.innerHTML = html;
