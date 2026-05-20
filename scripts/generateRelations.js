e71f8be1d7842187eca26d3a2caf1687ba659d25
Tuyan T <tuyantat@gmail.com>
2026-05-20 15:28:20 +0200

    version number update

--- scripts/generateRelations.js
+++ scripts/generateRelations.js
@@ -453,7 +453,7 @@ function addFooter() {
   const footer = document.createElement("div");
   footer.id = "websiteFooter";
   footer.innerHTML = `
-      <p>Website by Tuyan Tatliparmak v1.2.0</p>
+      <p>Website by Tuyan Tatliparmak v1.3.0</p>
       <div id="footerButtons">
         <a class="footerButton" href="https://www.linkedin.com/in/tuyan/"><i class="fa-brands fa-linkedin"></i></a>
         <a class="footerButton" href="https://github.com/TuT597"><i class="fa-brands fa-square-github"></i></a>
